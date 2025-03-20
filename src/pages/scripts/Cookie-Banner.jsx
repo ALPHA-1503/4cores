7ede9a31dcd2b000cdde46e5b77a010bac56af2b
arnau <arnaudv2003@gmail.com>
2025-03-20 17:00:44 +0100

    Updating cookie button.

--- src/pages/scripts/Cookie-Banner.jsx
+++ src/pages/scripts/Cookie-Banner.jsx
@@ -6,22 +6,6 @@ import 'simplebar-react/dist/simplebar.min.css';
 import {NavLink} from "react-router-dom";
 
 export default function CookieBanner() {
-    const cookiesData = [
-        {
-            name: 'cookieConsent',
-            domain: 'www.4cores.be',
-            expiration: '1 an',
-            description: 'Cookie utilisé pour vérifier le consentement de l\'utilisateur concernant l\'utilisation de cookies.'
-        },
-        {
-            name: '_ga',
-            domain: 'www.google.com',
-            expiration: '2 ans',
-            description: 'Cookies utilisés pour analyser le trafic au sein de notre site web. Ces cookies sont ' +
-                'supprimés si l\'utilisateur ne donne pas son consentement.'
-        },
-    ];
-
     const { handleCookieChoice, cookieConsent } = useContext(CookieContext);
     const [isVisible, setIsVisible] = useState(true);
     const [showCookieInfo, setShowCookieInfo] = useState(false);
@@ -60,38 +44,7 @@ export default function CookieBanner() {
                         <p>Ce site web utilise uniquement des cookies essentiels pour mémoriser votre langue par défaut et analyser son trafic.</p>
                     </div>
                     <div className="cookie_banner_mid_btm">
-                        {!showCookieInfo && (
-                            <a onClick={toggleCookieInfo} style={{ cursor: 'pointer' }}>Plus d'informations.</a>
-                        )}
-                        {showCookieInfo && (
-                            <div className="cookies-info">
-                                <a onClick={handleClose} style={{ cursor: 'pointer' }}>Fermer</a>
-                                <SimpleBar style={{height: '250px'}}>
-                                    <div className="cookie-table-container">
-                                        <table className="cookie-table">
-                                            <thead>
-                                            <tr>
-                                                <th>Cookie</th>
-                                                <th>Domaine</th>
-                                                <th>Description</th>
-                                                <th>Durée</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            {cookiesData.map((cookie, index) => (
-                                                <tr key={index}>
-                                                    <td>{cookie.name}</td>
-                                                    <td>{cookie.domain}</td>
-                                                    <td>{cookie.description}</td>
-                                                    <td>{cookie.expiration}</td>
-                                                </tr>
-                                            ))}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </SimpleBar>
-                            </div>
-                        )}
+                        <NavLink to="/cookie">En savoir plus.</NavLink>
                     </div>
                 </div>
                 <div className="cookie_banner_btm">
