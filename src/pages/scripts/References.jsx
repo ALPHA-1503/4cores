4e420e2ec6e7ec1d2ed84ee74e6ca7694c1628dd
Arnaud <etu48575@henallux.be>
2025-03-22 16:36:40 +0100

    Update

---
+++ src/pages/scripts/References.jsx
@@ -0,0 +1,11 @@
+export default function References(){
+
+    return(
+        <section className="references">
+            <h1>Références</h1>
+            <section className="references-banner">
+                
+            </section>
+        </section>
+    )
+}
